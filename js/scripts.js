fc8bc5856a613104bcb606c57545afb9fe48b2ce
Juan Camilo González <info@juancgonzalez.com>
2019-10-09 23:51:14 -0500

    aaa

--- js/scripts.js
+++ js/scripts.js
@@ -686,28 +686,28 @@ function dataURItoBlob(dataURI) {
 //   );
 // });
 
-// window.fbAsyncInit = function () {
-//   FB.init({
-//     appId: '481310545785663',
-//     cookie: true,
-//     xfbml: true,
-//     version: 'v2.0'
-//   });
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: '481310545785663',
+    cookie: true,
+    xfbml: true,
+    version: 'v4.0'
+  });
 
-//   FB.AppEvents.logPageView();
-// };
+  FB.AppEvents.logPageView();
+};
 
-// (function (d, s, id) {
-//   var js,
-//     fjs = d.getElementsByTagName(s)[0];
-//   if (d.getElementById(id)) {
-//     return;
-//   }
-//   js = d.createElement(s);
-//   js.id = id;
-//   js.src = 'https://connect.facebook.net/en_US/sdk.js';
-//   fjs.parentNode.insertBefore(js, fjs);
-// })(document, 'script', 'facebook-jssdk');
+(function (d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js';
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, 'script', 'facebook-jssdk');
 
 fbShare.addEventListener('click', function () {
   FB.login(
